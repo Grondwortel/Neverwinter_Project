b1a46a9433acadad0c882777c35c867030437de7
NielsKlop <niels_klop@hotmail.com>
2015-12-03 14:42:06 +0100

    Added simple form for Character Creation

---
+++ Neverwinter/Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Neverwinter.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Add()
+        {
+            // If user is logged in
+            return View();
+
+            // If user is NOT logged in
+            // return View(Home/Index)
+            // Including error Message
+        }
+    }
+}
---
+++ Neverwinter/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace Neverwinter.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public bool Gender { get; set; }
+        public string Class { get; set; }
+    }
+}
