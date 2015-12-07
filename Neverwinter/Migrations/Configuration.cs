d553031a2e21a2b7d5682099f0457616f6cef4a9
mattijn kleijer <mattijnkleijer@gmail.com>
2015-12-07 14:42:09 +0100

    changed gender class in database, edited character "Create" and "Edit" files so they work now with new values

--- Neverwinter/Migrations/Configuration.cs
+++ Neverwinter/Migrations/Configuration.cs
@@ -1,12 +1,15 @@
 namespace Neverwinter.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Neverwinter.Models.NeverwinterContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(Neverwinter.Models.NeverwinterContext context)
