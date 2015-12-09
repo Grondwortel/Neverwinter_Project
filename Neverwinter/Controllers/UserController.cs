561735d929a545f33f93b4972f95c5d80bba56ca
NielsKlop <niels_klop@hotmail.com>
2015-12-09 14:21:10 +0100

    Fixed User Creation

--- Neverwinter/Controllers/UserController.cs
+++ Neverwinter/Controllers/UserController.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using Neverwinter.Models;
 
 namespace Neverwinter.Controllers
 {
-    public class UsersController : Controller
+    public class UserController : Controller
     {
         private NeverwinterContext db = new NeverwinterContext();
 
-        // GET: Users
+        // GET: User
         public ActionResult Index()
         {
             return View(db.Users.ToList());
         }
 
-        // GET: Users/Details/5
+        // GET: User/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -35,24 +31,18 @@ namespace Neverwinter.Controllers
             return View(user);
         }
 
-        // GET: Users/Create
-        public ActionResult Register()
+        // GET: User/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        // GET: Users/Create
-        public ActionResult Add()
-        {
-            return View();
-        }
-
-        // POST: Users/Add
+        // POST: User/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Add([Bind(Include = "Id,FirstName,Surname,Email,Username,Password")] User user)
+        public ActionResult Create([Bind(Include = "Id,FirstName,SurName,Email,Password,Username")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -64,7 +54,7 @@ namespace Neverwinter.Controllers
             return View(user);
         }
 
-        // GET: Users/Edit/5
+        // GET: User/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -79,12 +69,12 @@ namespace Neverwinter.Controllers
             return View(user);
         }
 
-        // POST: Users/Edit/5
+        // POST: User/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,Tussenvoegsel,SirName,Email,Username")] User user)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,SurName,Email,Password,Username")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -95,7 +85,7 @@ namespace Neverwinter.Controllers
             return View(user);
         }
 
-        // GET: Users/Delete/5
+        // GET: User/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -110,7 +100,7 @@ namespace Neverwinter.Controllers
             return View(user);
         }
 
-        // POST: Users/Delete/5
+        // POST: User/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
--- Neverwinter/Models/User.cs
+++ Neverwinter/Models/User.cs
@@ -4,11 +4,15 @@ namespace Neverwinter.Models
 {
     public class User
     {
+        //System Info
         public int Id { get; set; }
 
+        //Basic Info
         public string FirstName { get; set; }
         public string SurName { get; set; }
+        public string Avatar { get; set; }
 
+        //Account Info
         public string Email { get; set; }
         public string Password { get; set; }
         public string Username { get; set; }
