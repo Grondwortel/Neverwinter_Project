33e450f4f46117427128eaebee30c99540ecc303
mattijn kleijer <mattijnkleijer@gmail.com>
2016-01-14 13:33:40 +0100

    added profession model

---
+++ Neverwinter/Models/Profession.cs
@@ -0,0 +1,9 @@
+﻿namespace Neverwinter.Models
+{
+    public class Profession
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Bonus { get; set; }
+    }
+}
